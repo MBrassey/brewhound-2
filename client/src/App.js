79121fd8a8086d930428adec38e327b560739587
Matthew O <mo@pop-os.localdomain>
2021-01-14 16:43:41 -0700

    refactored navbar with react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import SignUp from './pages/Signup';
-import Search from './pages/Search';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
+import Search from "./pages/Search";
+
 function App() {
+  // declare new apollo client and pulls token from localstorage if available
+  const client = new ApolloClient({
+    request: (operation) => {
+      const token = localStorage.getItem("id_token");
+
+      operation.setContext({
+        headers: {
+          authorization: token ? `Bearer ${token}` : "",
+        },
+      });
+    },
+
+    uri: "/graphql",
+  });
+
   return (
-
+    <ApolloProvider client={client}>
       <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/search" component={Search} />
-        </Switch>
-      </>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/search" component={Search} />
+          </Switch>
+        </>
       </Router>
-
-    
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,47 +1,171 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
-import { Container, Modal, Tab } from "react-bootstrap";
-import SignUpForm from "./SignupForm";
+import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
+import SignUp from "./SignupForm";
 import LoginForm from "./LoginForm";
+
 import Auth from "../utils/auth";
 
-const Nav = () => {
-  // set modal display state
+const AppNavbar = () => {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <header className="fixed-top header" id="header">
-      <div className="container-fluid">
-        <div className="row justify-content-center">
-          <div className="col-xl-10 d-flex align-items-center">
-            <h1 className="logo mr-auto">
-              <a>
-                <Link to="/">Brew Hound</Link>
-                <span>.</span>
-              </a>
-            </h1>
-            <nav className="nav-menu d-none d-lg-block">
-              <ul>
-                <li>
-                  <a href="#">ipsum</a>
-                </li>
-                <li>
-                  <a href="#">dolor</a>
-                </li>
-                <li>
-                  <a href="#">amet</a>
-                </li>
-              </ul>
-            </nav>
-            <Link className="get-started-btn" to="/login">
-              Login/SignUp
-            </Link>
-          </div>
-        </div>
-      </div>
-    </header>
+    <>
+      <Navbar bg="light" variant="light" expand="lg" id="header">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/" className="logo">
+            <span>b</span>rew<span>h</span>ound
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar" />
+          <Navbar.Collapse id="navbar">
+            <Nav className="ml-auto">
+              <Nav.Link as={Link} to="/">
+                Search For Breweries
+              </Nav.Link>
+              {Auth.loggedIn() ? (
+                <>
+                  <Nav.Link as={Link} to="/saved">
+                    Checkout Your Breweries
+                  </Nav.Link>
+                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                </>
+              ) : (
+                <Nav.Link
+                  className="get-started-btn"
+                  onClick={() => setShowModal(true)}
+                >
+                  Login/Sign Up
+                </Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* Modal  */}
+      <Modal
+        size="sm"
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby="login-signup-modal"
+      >
+        <Tab.Container defaultActiveKey="login">
+          <Modal.Header closeButton>
+            <Modal.Title id="signup-modal">
+              <Nav variant="tabs">
+                <Nav.Item>
+                  <Nav.Link eventKey="login" className="text-sec">
+                    Login
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="signup" className="text-sec">
+                    Sign Up
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey="login">
+                <LoginForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+              <Tab.Pane eventKey="signup">
+                <SignUp handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
+      </Modal>
+    </>
   );
 };
 
-export default Nav;
+export default AppNavbar;
+
+// import React from "react";
+
+// import { Link } from "react-router-dom";
+
+// const Nav = () => {
+//   return (
+//     <header className="fixed-top header" id="header">
+//       <div className="container-fluid">
+//         <div className="row justify-content-center">
+//           <div className="col-xl-10 d-flex align-items-center">
+//             <h1 className="logo mr-auto">
+//               <Link to="/">Brew Hound</Link>
+//               <span>.</span>
+//             </h1>
+//             <nav className="nav-menu d-none d-lg-block">
+//               <ul>
+//                 <li>
+//                   <a href="#">ipsum</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">dolor</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">amet</a>
+//                 </li>
+//               </ul>
+//             </nav>
+//             <Link className="get-started-btn" to="/login">
+//               Login/SignUp
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Nav;
+
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+
+// import { Container, Modal, Tab } from "react-bootstrap";
+// import SignUpForm from "./SignupForm";
+// import LoginForm from "./LoginForm";
+// import Auth from "../utils/auth";
+
+// const Nav = () => {
+//   // set modal display state
+//   const [showModal, setShowModal] = useState(false);
+
+//   return (
+//     <header className="fixed-top header" id="header">
+//       <div className="container-fluid">
+//         <div className="row justify-content-center">
+//           <div className="col-xl-10 d-flex align-items-center">
+//             <h1 className="logo mr-auto">
+//               <a>
+//                 <Link to="/">Brew Hound</Link>
+//                 <span>.</span>
+//               </a>
+//             </h1>
+//             <nav className="nav-menu d-none d-lg-block">
+//               <ul>
+//                 <li>
+//                   <a href="#">ipsum</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">dolor</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">amet</a>
+//                 </li>
+//               </ul>
+//             </nav>
+//             <Link className="get-started-btn" to="/login">
+//               Login/SignUp
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Nav;
