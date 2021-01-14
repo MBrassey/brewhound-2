9d84c78f24b533870adcbc5cb0127092ea4ace12
Matthew O <mo@pop-os.localdomain>
2021-01-14 16:25:13 -0700

    added yelp to client side

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -22,7 +22,6 @@ const AppNavbar = () => {
               <Nav.Link as={Link} to="/">
                 Search For Breweries
               </Nav.Link>
-              {/* if user is logged in show saved books and logout */}
               {Auth.loggedIn() ? (
                 <>
                   <Nav.Link as={Link} to="/saved">
@@ -42,14 +41,13 @@ const AppNavbar = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      {/* set modal data up */}
+      {/* Modal  */}
       <Modal
-        size="lg"
+        size="sm"
         show={showModal}
         onHide={() => setShowModal(false)}
         aria-labelledby="signup-modal"
       >
-        {/* tab container to do either signup or login component */}
         <Tab.Container defaultActiveKey="login">
           <Modal.Header closeButton>
             <Modal.Title id="signup-modal">
