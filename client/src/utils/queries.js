9bd548205dd41210d624ba7f59f521370c81d502
MBrassey <luc1d_life@protonmail.com>
2021-01-15 19:07:40 -0700

    + meQuery

---
+++ client/src/utils/queries.js
@@ -0,0 +1,15 @@
+import gql from "graphql-tag";
+
+export const GET_USER = gql`
+  {
+    me {
+      _id
+      username
+      email
+      brewCount
+      savedBreweries {
+        brewId
+      }
+    }
+  }
+`;
