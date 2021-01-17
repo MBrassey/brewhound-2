e98a8ff34dc8d64a4d1671ef53e05b98da5ec980
ry brown <ryan.alex.brown@gmail.com>
2021-01-16 20:41:55 -0700

    cleaned up some code, got rid of a few warnings

--- client/src/components/Page-top.js
+++ client/src/components/Page-top.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Button } from 'react-bootstrap';
+
 
 const TopArrow = () =>{
     const [showScroll, setShowScroll] = useState(false)
--- client/src/pages/About-us.js
+++ client/src/pages/About-us.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
-import image from "../img/pexels-pixabay-159291.jpg"
+
 
 const AboutUs = () => {
     return (
