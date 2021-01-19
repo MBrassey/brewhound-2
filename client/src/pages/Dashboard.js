e930d91d98e9e42d6ea766c38d112835ceff2e45
MBrassey <luc1d_life@protonmail.com>
2021-01-18 19:35:42 -0700

    Implemented List of Saved Breweries

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,39 +1,39 @@
-// import { CardColumns, Card, Button } from "react-bootstrap";
-import { useQuery } from "@apollo/react-hooks";
-// import { removeBreweryId } from "../utils/localStorage";
-// import { REMOVE_BREWERY } from "../utils/mutations";
+import { CardColumns, Card, Button } from "react-bootstrap";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { removeBreweryId } from "../utils/localStorage";
+import { REMOVE_BREWERY } from "../utils/mutations";
 import { GET_USER } from "../utils/queries";
 import FadeIn from "react-fade-in";
-// import Auth from "../utils/auth";
-import React from "react";
+import Auth from "../utils/auth";
+import React, { useState } from "react";
 
 const Dashboard = () => {
   const { loading, data } = useQuery(GET_USER);
   const userData = data?.me || {};
   console.log("test:" + userData.username);
+  const [showModal, setShowModal] = useState(false);
+  const [removeBrew, { error }] = useMutation(REMOVE_BREWERY);
+  const handleDeleteBrew = async (brewId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  // const [removeBrew, { error }] = useMutation(REMOVE_BREWERY);
-  // const handleDeleteBrew = async (brewId) => {
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      return false;
+    }
 
-  //   if (!token) {
-  //     return false;
-  //   }
+    try {
+      await removeBrew({
+        variables: { brewId },
+      });
 
-  //   try {
-  //     await removeBrew({
-  //       variables: { brewId },
-  //     });
+      if (error) {
+        throw new Error("Something went wrong!");
+      }
 
-  //     if (error) {
-  //       throw new Error("Something went wrong!");
-  //     }
-
-  //     removeBreweryId(brewId);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
+      removeBreweryId(brewId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   if (loading) {
     return <h2>LOADING...</h2>;
@@ -65,6 +65,47 @@ const Dashboard = () => {
                         : "You have no saved breweries!"}
                     </h2>
                   </div>
+                  <CardColumns className="mt-4">
+                    {userData.savedBreweries.map((brews) => {
+                      return (
+                        <Card key={brews.brewId}>
+                          {brews.photo ? (
+                            <Card.Img
+                              src={brews.photo}
+                              alt={`${brews.name} Yelp cover`}
+                              variant="top"
+                            />
+                          ) : null}
+
+                          <Card.Body>
+                            <Card.Title className="BrewFont">
+                              {brews.name}
+                            </Card.Title>
+                            <Card.Text>Rating: {brews.rating}</Card.Text>
+                            <Card.Link href={brews.link} target="_blank">
+                              {brews.name}'s Website
+                            </Card.Link>
+                            <Card.Text>Location: {brews.location}</Card.Text>
+                            {Auth.loggedIn() && (
+                              <Button
+                                className="btn-block btn-danger"
+                                onClick={() => handleDeleteBrew(brews.brewId)}
+                              >
+                                Delete this Book!
+                              </Button>
+                            )}
+                            <Button
+                              variant="warning"
+                              className="btn-block btn-info"
+                              onClick={() => setShowModal(true)}
+                            >
+                              Map
+                            </Button>
+                          </Card.Body>
+                        </Card>
+                      );
+                    })}
+                  </CardColumns>
                 </div>
               </div>
             </div>
